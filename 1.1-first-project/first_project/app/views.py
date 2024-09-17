be57e63e11ed21470e35d17c6334cf891d51fc61
Roman Podkorytov <romiraxp@mail.ru>
2024-09-17 10:11:24 +0500

    Hometask

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,6 +1,7 @@
+import datetime
+import os
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
-
 
 def home_view(request):
     template_name = 'app/home.html'
@@ -8,8 +9,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,7 +24,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +33,10 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    files = os.listdir(dir_path)
+    files = [f for f in files if os.path.isfile(dir_path + '/' + f)]
+    file_list = ' '.join(files)
+    msg = f'Список файлов: {file_list}'
+    return HttpResponse(msg)
+    #raise NotImplemented
