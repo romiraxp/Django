a3121ca3fb6627bf5f2111cdf8bcbcaa35782831
Roman Podkorytov <romiraxp@mail.ru>
2024-11-05 15:12:16 +0500

    DRF stocks&products

--- 3.2-crud/stocks_products/logistic/models.py
+++ 3.2-crud/stocks_products/logistic/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=60, unique=True)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=60, unique=True, verbose_name="Продукт")
+    description = models.TextField(null=True, blank=True,verbose_name="Описание")
 
 
 class Stock(models.Model):
-    address = models.CharField(max_length=200, unique=True)
+    address = models.CharField(max_length=200, unique=True, verbose_name="Адрес")
     products = models.ManyToManyField(
         Product,
         through='StockProduct',
--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,24 +1,34 @@
+from django.db import transaction
 from rest_framework import serializers
-
+from .models import Product, Stock, StockProduct
 
 class ProductSerializer(serializers.ModelSerializer):
+#    positions = ProductPositionSerializer(many=True)
     # настройте сериализатор для продукта
-    pass
-
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
 
 class ProductPositionSerializer(serializers.ModelSerializer):
+    #positions = ProductSerializer()
     # настройте сериализатор для позиции продукта на складе
-    pass
-
+    class Meta:
+        model = StockProduct
+#        fields = ['product', 'quantity', 'price', 'positions']
+        fields = ['product', 'quantity', 'price']
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
+    @transaction.atomic
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
+        positions = validated_data.pop('positions', [])
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
@@ -26,18 +36,42 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        StockProduct.objects.bulk_create([StockProduct(stock=stock, **position) for position in positions])
+        # for position in positions:
+        #     StockProduct.objects.create(stock=stock, **position)
 
         return stock
 
+    @transaction.atomic
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
-        positions = validated_data.pop('positions')
-
+        # способ Александра это удалить и заново создать с 0
+        positions = validated_data.pop('positions', [])
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        instance.positions.all().delete()
+        StockProduct.objects.bulk_create([StockProduct(stock=stock, **position) for position in positions])
         return stock
+
+        # positions_data = validated_data.pop('positions', [])
+        # positions = (instance.positions).all()
+        # positions = list(positions)
+        # print(list(positions_data))
+        # print(positions)
+        # instance.address = validated_data.get('address', instance.address)
+        # print(instance.address)
+        # instance.quantity = validated_data.get('quantity', instance.quantity)
+        # instance.price = validated_data.get('price', instance.price)
+        # instance.save()
+
+        # for position_data in positions_data:
+        #     position = positions.pop(0)
+        #     position.product = position_data.get('product', position.product)
+        #     position.quantity = position_data.get('quantity', position.quantity)
+        #     position.price = position_data.get('price', position.price)
+        #     position.save()
+            # StockProduct.objects.update(stock=stock, **position)
+        # return instance
